26122d2e43122cd1c50a83e059d30c5302c127c0
Sidam31 <69627321+Sidam31@users.noreply.github.com>
2022-07-10 13:49:57 +0200

    several urls + csv in utf8 + lint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from genericpath import exists
 from bs4 import BeautifulSoup
 
 import urllib.parse
@@ -12,9 +13,18 @@ import sys
 import re
 import os
 
+
 class DB:
     path = os.path.dirname(os.path.realpath(__file__))
-    con = sqlite3.connect(path + '/db.sqlite3')
+    database_path = os.path.join(path, 'db_test.sqlite3')
+    old_database_path = database_path.replace('db', 'db_old')
+    if exists(old_database_path):
+        os.remove(old_database_path)
+    if exists(database_path):
+        os.rename(database_path, old_database_path)
+        old_con = sqlite3.connect(old_database_path)
+        old_cur = old_con.cursor()
+    con = sqlite3.connect(database_path)
     cur = con.cursor()
 
     def update():
@@ -38,10 +48,21 @@ class Family:
         return Family.instances[family_id] if family_id in Family.instances.keys() else Family(father_permalink, mother_permalink)
 
     def save(self):
-        DB.cur.execute('INSERT INTO family (id, father_permalink, mother_permalink, wedding_date, wedding_place) VALUES (?, ?, ?, ?, ?)', (self.id, self.father_permalink, self.mother_permalink, self.wedding_date, self.wedding_place))
+        DB.cur.execute('INSERT INTO family (id, father_permalink, mother_permalink, wedding_date, wedding_place) VALUES (?, ?, ?, ?, ?)',
+                       (self.id, self.father_permalink, self.mother_permalink, self.wedding_date, self.wedding_place))
+
 
 class People:
-    def __init__(self, permalink, firstname = '', lastname = '', sex = '', birthdate = '', birthplace = '', deathdate = '', deathplace = '', family_id = ''):
+    def __init__(self,
+                 permalink,
+                 firstname='',
+                 lastname='',
+                 sex='',
+                 birthdate='',
+                 birthplace='',
+                 deathdate='',
+                 deathplace='',
+                 family_id=''):
         self.permalink = permalink
         self.firstname = firstname
         self.lastname = lastname
@@ -57,7 +78,8 @@ class People:
         return ' '.join(map(str, (self.sex, self.firstname, self.lastname, self.permalink, self.birthdate, self.deathdate, self.birthplace, self.deathplace)))
 
     def save(self, DB):
-        DB.cur.execute('INSERT INTO people (firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (self.firstname, self.lastname, self.sex, self.birthdate, self.birthplace, self.deathdate, self.deathplace, self.permalink))
+        DB.cur.execute('INSERT INTO people (firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                       (self.firstname, self.lastname, self.sex, self.birthdate, self.birthplace, self.deathdate, self.deathplace, self.permalink))
 
 
 class Process:
@@ -71,9 +93,11 @@ class Process:
         if not len(self.cache) and os.path.isfile(self.filename) and os.path.getmtime(self.filename) > time.time() - 12 * 3600 and os.path.getsize(self.filename) > 0:
             with open(self.filename, 'r', encoding='utf-8') as f:
                 cache = json.load(f)
-                DB.cur.execute('SELECT firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id FROM people')
+                DB.cur.execute(
+                    'SELECT firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id FROM people')
                 for (firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id) in DB.cur.fetchall():
-                    people = People(permalink, firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, family_id)
+                    people = People(permalink, firstname, lastname, sex,
+                                    birthdate, birthplace, deathdate, deathplace, family_id)
                     for (k, v) in cache.items():
                         if v == people.permalink:
                             self.cache[k] = people
@@ -82,12 +106,15 @@ class Process:
     def save_caches(self):
         with open(self.filename, 'w') as f:
             for (k, v) in self.cache.items():
-                self.cache[k] = v.permalink
+                if isinstance(v,str):
+                    self.cache[k] = v
+                else:
+                    self.cache[k] = v.permalink
             json.dump(self.cache, f)
 
     def extractParams(href):
         str1 = Process.extractQuery(href)
-        return {x[0] : x[1] for x in [x.split("=") for x in str1[1:].split(";") ]}
+        return {x[0]: x[1] for x in [x.split("=") for x in str1[1:].split(";")]}
 
     def extractQuery(href):
         parts = href.split('?')
@@ -104,19 +131,28 @@ class Process:
         response = requests.get(Process.base + path)
         parts = response.text.split('<h3')
         soup = BeautifulSoup(parts[0], "html.parser")
-        permalink_ = soup.select('h1 input')[0]['value'].strip() if len(soup.select('h1 input')) > 0 else ''
+        permalink_ = soup.select('h1 input')[0]['value'].strip() if len(
+            soup.select('h1 input')) > 0 else ''
         parts = permalink_.replace('[', '').replace(']', '').split('/')
-        permalink = ('p=%s;n=%s;' % (parts[0], parts[1]) + ('oc=%s' % (parts[2],) if parts[2] !='0' else '')) if len(parts) > 2 else ''
+        permalink = ('p=%s;n=%s;' % (parts[0], parts[1]) + ('oc=%s' % (
+            parts[2],) if parts[2] != '0' else '')) if len(parts) > 2 else ''
         people = People(permalink)
-        people.sex = soup.select('h1 img')[0]['alt'].strip() if len(soup.select('h1 img')) > 0 else ''
-        people.firstname = soup.select('h1 a')[0].text.strip() if len(soup.select('h1 a')) > 0 else ''
-        people.lastname = soup.select('h1 a')[1].text.strip() if len(soup.select('h1 a')) > 1 else ''
-        dict1 = Process.extractParams(soup.select('ul li a.date')[0]['href'].strip()) if len(soup.select('ul li a.date')) > 0 else {}
+        people.sex = soup.select('h1 img')[0]['alt'].strip() if len(
+            soup.select('h1 img')) > 0 else ''
+        people.firstname = soup.select('h1 a')[0].text.strip() if len(
+            soup.select('h1 a')) > 0 else ''
+        people.lastname = soup.select('h1 a')[1].text.strip() if len(
+            soup.select('h1 a')) > 1 else ''
+        dict1 = Process.extractParams(soup.select('ul li a.date')[
+                                      0]['href'].strip()) if len(soup.select('ul li a.date')) > 0 else {}
         people.birthdate = Process.dictToDate(dict1)
-        dict2 = Process.extractParams(soup.select('ul li a.date')[1]['href'].strip()) if len(soup.select('ul li a.date')) > 1 else {}
+        dict2 = Process.extractParams(soup.select('ul li a.date')[
+                                      1]['href'].strip()) if len(soup.select('ul li a.date')) > 1 else {}
         people.deathdate = Process.dictToDate(dict2)
-        people.birthplace = soup.select('ul li script')[0].text.strip().split('"')[1] if len(soup.select('ul li script')) > 0 else ''
-        people.deathplace = soup.select('ul li script')[1].text.strip().split('"')[1] if len(soup.select('ul li script')) > 1 else ''
+        people.birthplace = soup.select('ul li script')[0].text.strip().split('"')[
+            1] if len(soup.select('ul li script')) > 0 else ''
+        people.deathplace = soup.select('ul li script')[1].text.strip().split('"')[
+            1] if len(soup.select('ul li script')) > 1 else ''
         print(people)
         people.save(DB)
         self.cache[path] = people
@@ -126,28 +162,37 @@ class Process:
         if parents:
             ul = parents.findNext('ul')
             links = ul.findAll('li')
-            father_ = Process.extractQuery(links[0].find('a')['href'].strip()) if len(links) > 0 else ''
+            father_ = Process.extractQuery(links[0].find(
+                'a')['href'].strip()) if len(links) > 0 else ''
             if father_:
-                father = self.cache[father_] if father_ in self.cache.keys() else self.browse(father_)
+                father = self.cache[father_] if father_ in self.cache.keys(
+                ) else self.browse(father_)
             father_permalink = father.permalink if father else ''
-            mother_ = Process.extractQuery(links[1].find('a')['href'].strip()) if len(links) > 1 else ''
+            mother_ = Process.extractQuery(links[1].find(
+                'a')['href'].strip()) if len(links) > 1 else ''
             if mother_:
-                mother = self.cache[mother_] if mother_ in self.cache.keys() else self.browse(mother_)
+                mother = self.cache[mother_] if mother_ in self.cache.keys(
+                ) else self.browse(mother_)
             mother_permalink = mother.permalink if mother else ''
             if father_permalink or mother_permalink:
                 family = Family.get(father_permalink, mother_permalink)
                 family.save()
-                DB.cur.execute('UPDATE people SET family_id = ? WHERE permalink = ?', (family.id, people.permalink))
-        spouses = soup.find('h3', text='Spouses and children')
+                DB.cur.execute(
+                    'UPDATE people SET family_id = ? WHERE permalink = ?', (family.id, people.permalink))
+        spouses = soup.find('h3', text='Spouses and children') or soup.find(
+            'h3', text='Mariages et enfants')
         if spouses:
             ul = spouses.findNext('ul')
             links = ul.findAll('b')
-            spouse_ = Process.extractQuery(links[0].find('a')['href'].strip()) if len(links) > 0 else ''
+            spouse_ = Process.extractQuery(links[0].find(
+                'a')['href'].strip()) if len(links) > 0 else ''
             if spouse_ and spouse_ not in self.cache.keys():
                 spouse = self.browse(spouse_)
-                dict1 = Process.extractParams(ul.select('li a.date')[0]['href'].strip()) if len(ul.select('li a.date')) > 0 else {}
+                dict1 = Process.extractParams(ul.select('li a.date')[0]['href'].strip()) if len(
+                    ul.select('li a.date')) > 0 else {}
                 wedding_date = Process.dictToDate(dict1)
-                wedding_place = ul.select('li script')[0].text.strip().split('"')[1] if len(ul.select('li script')) > 0 else ''
+                wedding_place = ul.select('li script')[0].text.strip().split('"')[
+                    1] if len(ul.select('li script')) > 0 else ''
                 father_permalink = people.permalink if people.sex == 'M' else spouse.permalink
                 mother_permalink = spouse.permalink if people.sex == 'M' else people.permalink
                 family = Family.get(father_permalink, mother_permalink)
@@ -158,31 +203,40 @@ class Process:
         return people
 
     def export(self, filename):
-        with open(filename, 'w') as f:
-            DB.cur.execute('SELECT firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id FROM people')
-            f.write('person,grampsid,firstname,lastname,gender,note,birthdate,birthplace,deathdate,deathplace\n')
+        with open(filename, 'w', encoding='utf-8') as f:
+            DB.cur.execute(
+                'SELECT firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id FROM people')
+            f.write(
+                'person,grampsid,firstname,lastname,gender,note,birthdate,birthplace,deathdate,deathplace\n')
             people = DB.cur.fetchall()
             for (firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id) in people:
                 sex = 'male' if sex == 'M' else 'female' if sex == 'F' else ''
                 source = Process.base + permalink
-                f.write('%s,,"%s","%s",%s,%s,%s,"%s",%s,"%s"\n' % (permalink, firstname, lastname, sex, source, birthdate, birthplace, deathdate, deathplace))
+                f.write('%s,,"%s","%s",%s,%s,%s,"%s",%s,"%s"\n' % (
+                    permalink, firstname, lastname, sex, source, birthdate, birthplace, deathdate, deathplace))
             f.write('\n\nmarriage,husband,wife,date,place,source\n')
-            DB.cur.execute('SELECT id, father_permalink, mother_permalink, wedding_date, wedding_place FROM family')
+            DB.cur.execute(
+                'SELECT id, father_permalink, mother_permalink, wedding_date, wedding_place FROM family')
             for (family_id, father_permalink, mother_permalink, wedding_date, wedding_place) in DB.cur.fetchall():
-                f.write('%s,%s,%s,%s,"%s"\n' % (family_id, father_permalink, mother_permalink, wedding_date or '', wedding_place or ''))
+                f.write('%s,%s,%s,%s,"%s"\n' % (family_id, father_permalink,
+                        mother_permalink, wedding_date or '', wedding_place or ''))
             f.write('\n\nfamily,child\n')
             for (firstname, lastname, sex, birthdate, birthplace, deathdate, deathplace, permalink, family_id) in people:
                 f.write('%s,%s\n' % (family_id or '', permalink))
+
 
 if __name__ == '__main__':
     DB.update()
     process = Process('cache.json')
     process.init_caches()
-    url = sys.argv[1] if len(sys.argv) > 1 else ''
-    if (url):
-        process.browse(url.replace(Process.base, ''))
-        DB.con.commit()
-        process.save_caches()
+    if len(sys.argv) > 1:
+        for i_url in range(1, len(sys.argv)):
+            url = sys.argv[i_url]
+
+            process.browse(url.replace(Process.base, ''))
+            DB.con.commit()
+            process.save_caches()
+
         process.export('export.csv')
     else:
         print('Please provide a URL')
